7fbada674aa4dcec21239aae1d0adf42fef1077a
Philip Iezzi <philip@onlime.ch>
2024-03-14 16:40:01 +0100

    Simplify PendingRequestMixinTest by invading `HandlerStack::findByName()`

--- tests/PendingRequestMixinTest.php
+++ tests/PendingRequestMixinTest.php
@@ -9,19 +9,15 @@ use Onlime\LaravelHttpClientGlobalLogger\Mixins\PendingRequestMixin;
 it('rebuilds the guzzle handler stack to include the logger middleware at the bottom of the stack', function () {
     PendingRequest::mixin(new PendingRequestMixin);
 
-    /** @var PendingRequest $pendingRequest */
     $pendingRequest = Http::withHeaders(['X-Test' => 'true'])->log();
 
     /** @var HandlerStack $handler */
     $handler = $pendingRequest->getOptions()['handler'];
-    expect($handler)->toBeInstanceOf(HandlerStack::class);
 
-    // We need to invade the HandlerStack to access the stack property
-    $stack = invade($handler)->stack;
-    // The first key is the middlware, and the second key is the name of the middleware
-    $middlewareNames = Arr::pluck($stack, 1);
-
-    expect($middlewareNames)->toContain('http-client-global-logger-0')
-        ->and($middlewareNames)->toContain('http-client-global-logger-1')
-        ->and($middlewareNames)->not->toContain('http-client-global-logger-2');
+    // We need to invade the HandlerStack to access the stack property or findByName method
+    expect($handler)->toBeInstanceOf(HandlerStack::class)
+        ->and(invade($handler)->findByName('http-client-global-logger-0'))->toBeInt()
+        ->and(invade($handler)->findByName('http-client-global-logger-1'))->toBeInt()
+        ->and(fn () => invade($handler)->findByName('http-client-global-logger-2'))
+        ->toThrow(\InvalidArgumentException::class);
 });
